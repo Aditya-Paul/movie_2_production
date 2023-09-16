e03f42675e44129dd836d5b6de03c4f0d3314068
Aditya paul <adityapaul.dibbo25@gmail.com>
2023-09-16 10:44:14 +0600

    use functinality on json data

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       {/* for home section*/}
       <div className='main_container'>
         <h1>first commit</h1>
+        <h1>Seond</h1>
         <Home></Home>
       </div>
 
